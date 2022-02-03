d9a11776621f7c359e190a09cd9d346d44c565b7
pokemonChampion5833 <aahancharak@gmail.com>
2022-02-03 23:23:40 +0530

    feat : changed folder structure and added script to automatically rename images

---
+++ misc/rename.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#get current directory
+cur_dir="$(pwd)"
+
+#get filename
+echo "Enter folder name"
+read folder_name
+
+printf "\n"
+
+#check if folder is given
+if [ -z "$folder_name" ] || [ "$folder_name" = " " ]; 
+then
+    printf "Err! Directory cannot be empty\n"
+    exit 1
+fi
+
+#check if the given folder exists in the current directory
+if [ ! -d "$cur_dir/$folder_name" ];
+then
+    printf "Error! Specified directory cannot be found.\n"
+    exit 1
+fi
+
+#loop and change filenames in the folder if folder exists
+cd $folder_name
+
+idx=1;
+for FILE in * ; 
+
+do
+mv  "$FILE" "$idx.jpg" 
+((idx=idx+1))
+
+done;
+
+
+printf "Processed a total of {$idx} files\n"
+
